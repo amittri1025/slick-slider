c281e4fcb0ee6433b50116eb16828a02ec23e0d4
amittri1025 <74503582+amittri1025@users.noreply.github.com>
2021-05-10 01:01:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+$('.my-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay : true,
+    autoplaySpeed: 2000,
+    dots: false,
+    centerMode: true,
+    centerPadding: '0px',
+
+  });
+          
